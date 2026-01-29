8b84f3b24414513ed20a7cc50d42878f5bf2225b
Abdo Hamza <ah2929293@gmail.com>
2026-01-29 21:52:21 +0200

    From Abdo

--- lib/features/widgets/screens/home_screen.dart
+++ lib/features/widgets/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:card_game_app/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [CustomAppBar()])),
+      body: SafeArea(
+        child: Column(
+          children: [
+            CustomAppBar(),
+            SizedBox(height: 100.h),
+            Transform.rotate(
+              angle: 0.5,
+              child: Container(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center, // center them horizontally
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      child: Expanded(
+                        child: Image.asset(
+                          "assets/images/paper_plane.png",
+                          opacity: const AlwaysStoppedAnimation(.5),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      child: Expanded(
+                        child: Image.asset(
+                          "assets/images/paper_plane.png",
+                          opacity: const AlwaysStoppedAnimation(.5),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      child: Expanded(
+                        child: Image.asset(
+                          "assets/images/paper_plane.png",
+                          opacity: const AlwaysStoppedAnimation(.5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
